e96d5a9499f6d54feea746e0ca058794579f97c4
wl <1051671645@qq.com>
2026-03-08 21:36:35 +0800

    初始化

---
+++ src/main/java/com/wyc/wycaicode/WycAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.wyc.wycaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WycAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WycAiCodeApplication.class, args);
+    }
+
+}
