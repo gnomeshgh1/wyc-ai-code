067e0b876ce4af7d07f29387c73aa35626842f80
wl <1051671645@qq.com>
2026-03-30 22:23:18 +0800

    原生ai应用开发

---
+++ src/test/java/com/wyc/wycaicode/AiCodeGeneratorFacadeTest.java
@@ -0,0 +1,23 @@
+package com.wyc.wycaicode;
+
+import com.wyc.wycaicode.core.AiCodeGeneratorFacade;
+import com.wyc.wycaicode.model.enums.CodeGenTypeEnum;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+@SpringBootTest
+class AiCodeGeneratorFacadeTest {
+
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
+
+    @Test
+    void generateAndSaveCode() {
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("任务记录网站", CodeGenTypeEnum.MULTI_FILE);
+        Assertions.assertNotNull(file);
+    }
+}
